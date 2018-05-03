e1c2011785f4164e6f0551242bbce7b7381fd620
Zaedlen <34587930+Zaedlen@users.noreply.github.com>
2018-05-03 21:00:45 +0200

    Gen Nivel

--- Programacion/GeneracionNivel/src/generacionnivel/Juego.java
+++ Programacion/GeneracionNivel/src/generacionnivel/Juego.java
@@ -10,8 +10,8 @@ public class Juego extends BasicGame{
     private Input entrada;
     
     private Rectangle hab;
-    private Rectangle[] casillas = new Rectangle[64];
-    private boolean[] colorear = new boolean[64];
+    private Rectangle[][] casillas = new Rectangle[8][8];
+    private boolean[][] colorear = new boolean[8][8];
     
     public Juego(String t) throws SlickException{
         super(t);
@@ -25,7 +25,9 @@ public class Juego extends BasicGame{
         entrada = new Input(700);
         
         hab = new Rectangle(250,150,300,80);
-        for(int i=0;i<casillas.length;i++) casillas[i] = new Rectangle(((i%8)*160),((i/8)*80),160,80);
+        for(int i=0;i<casillas.length;i++)
+            for(int j=0;j<casillas[i].length;j++)
+                casillas[i][j] = new Rectangle(((i%8)*160),((j%8)*80),160,80);
         generacion();
     }
 
@@ -37,10 +39,11 @@ public class Juego extends BasicGame{
     @Override
     public void render(GameContainer container, Graphics g) throws SlickException {
         for(int i=0;i<casillas.length;i++){
-            if(colorear[i]) g.fill(casillas[i]);
-            else g.draw(casillas[i]);
-        }
-        
+            for(int j=0;j<casillas[i].length;j++){
+                if(colorear[i][j]) g.fill(casillas[i][j]);
+                else g.draw(casillas[i][j]);
+            }
+        }        
     }
     
     /**
@@ -59,13 +62,20 @@ public class Juego extends BasicGame{
         }*/
         
         //Aprox 2: decidir la celda superior tambien
-        colorear = new boolean[colorear.length];
+        /*colorear = new boolean[colorear.length];
         colorear[0]=true;
         for(int i=1;i<colorear.length;i++){
             if(!colorear[i]){
                 if((((int)(Math.random()*100))%2)!=0) colorear[i] = true;
                 if((((int)(Math.random()*100))%2)!=0) if((i+8)<colorear.length) colorear[i+8] = true;
             }
-        }
+        }*/
+        
+        //Aprox 3: decidir la celda superior tambien
+        int contador=1;
+        //colorear = new boolean[colorear.length];
+        //colorear[0]=true;
+        //if((contador==1))
+        for(int i=0;i<colorear.length;i++) colorear[i][0]=true;
     }
 }
