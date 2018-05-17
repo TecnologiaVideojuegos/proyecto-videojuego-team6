fc77df9111ea825ccb41cd1a3dc72e254d7a171b
Zaedlen <34587930+Zaedlen@users.noreply.github.com>
2018-05-17 20:21:58 +0200

    Documentacion de las clases de generacion

--- Programacion/ShutterEarth/src/shutterearth/map/randomGenerator/Salida.java
+++ Programacion/ShutterEarth/src/shutterearth/map/randomGenerator/Salida.java
@@ -7,12 +7,29 @@ import org.newdawn.slick.geom.Rectangle;
 import shutterearth.Game;
 import shutterearth.Media;
 
+/**
+ * Clase que guarda una referencia de la Habitacion a la que conduce desde la
+ * Habitacion en la que se encuentra. Permite evaluar mediante interseccion o
+ * colision si el personaje se encuentra en una salida
+ */
 public class Salida extends Rectangle
 {
+    /**Referencia a la Habitacion a la que conduce la Salida desde la Habitacion en la que se encuentra*/
     private Habitacion next;
+    /**Estos dos atributos permiten renderizar la imagen de la salida en el nivel: las escaleras*/
     private final Rectangle salida;
+    /**Estos dos atributos permiten renderizar la imagen de la salida en el nivel: las escaleras*/
     private final Image imagen;
 
+    /**
+     * Constructor. Genera una salida en la posicion adecuada que conduce o guarda
+     * como referencia la Habitacion next
+     * @param next
+     * @param x
+     * @param y
+     * @param width
+     * @param height 
+     */
     public Salida(Habitacion next, float x, float y, float width, float height) {
         super(x, y, width, height);
         this.next = next;
@@ -20,14 +37,27 @@ public class Salida extends Rectangle
         salida = new Rectangle(getX(), next.getY()-Game.getY()/32, getWidth(), (Game.step())+Game.getY()/41);//Solo se usa en salidas INF
     }
 
+    /**
+     * Devuelve la Habitacion a la que conduce la Salida
+     * @return 
+     */
     public Habitacion getNext() {
         return next;
     }
     
+    /**
+     * Modifica la referencia de la Habitacion a la que conduce la Salida
+     * @param h 
+     */
     public void setHab(Habitacion h){
         next = h;
     }
     
+    /**
+     * Permite renderizar la Salida
+     * @param g
+     * @param b 
+     */
     public void render(Graphics g, boolean b)
     {
         if(!b){
